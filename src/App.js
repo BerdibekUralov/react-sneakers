b76c5c747b3a6887f25624efc04a87d7832202e7
BerdibekUralov <uralov0212@gmail.com>
2022-11-08 09:03:14 +0300

    side bar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import 'macro-css';
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <div>
+            <h3>Корзина</h3>
+          </div>
+
+          <div className="mt-40">
+            <div className="cartItem d-flex align-center">
+              <img className="cartItemImg" src="./img/sneakers/1.jpg" alt="Product" />
+              <div>
+                <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="./img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={60} src="./img/logo.png" alt="Logo" className="mr-10" />
@@ -12,10 +31,13 @@ function App() {
             <p className="opacity-6">Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="d-flex align-center">
-          <li className="mr-20 d-flex align-center">
+        <ul className="headerRight d-flex align-center justify-between">
+          <li className="d-flex align-center">
             <img width={18} src="./img/cart.svg" alt="Cart" />
             <span>1205 руб.</span>
+          </li>
+          <li className="d-flex align-center">
+            <img width={18} src="./img/heart.svg" alt="User" />
           </li>
           <li className="d-flex align-center">
             <img width={18} src="./img/user.svg" alt="User" />
@@ -23,11 +45,18 @@ function App() {
         </ul>
       </header>
 
-      <div className="content p-40">
-        <h2>Все кроссовки</h2>
+      <div className="content pl-40 pr-40">
+        <div className="d-flex justify-between align-center mb-40">
+          <h2>Все кроссовки</h2>
+          <div className="search-block">
+            <img src="./img/search.svg" alt="Search..." />
+            <input placeholder="Search..." /> 
+          </div>
+        </div>
         
         <div className="d-flex justify-between">
           <div className="card">
+            <img className="favorite" src="./img/unliked.svg" alt="Unlikded" />
             <img width={133} height={122} src="./img/sneakers/1.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between">
@@ -37,12 +66,13 @@ function App() {
               </div>
               
               <button>
-                <img src="./img/plus.svg" alt="" />
+                <img className="plus" src="./img/plus.svg" alt="" />
               </button>
             </div>
           </div>
 
           <div className="card">
+            <img className="favorite" src="./img/unliked.svg" alt="Unlikded" />
             <img width={133} height={122} src="./img/sneakers/2.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between">
@@ -52,12 +82,13 @@ function App() {
               </div>
               
               <button>
-                <img src="./img/plus.svg" alt="" />
+                <img className="plus" src="./img/plus.svg" alt="" />
               </button>
             </div>
           </div>
 
           <div className="card">
+            <img className="favorite" src="./img/unliked.svg" alt="Unlikded" />
             <img width={133} height={122} src="./img/sneakers/3.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between">
@@ -67,12 +98,13 @@ function App() {
               </div>
               
               <button>
-                <img src="./img/plus.svg" alt="" />
+                <img className="plus" src="./img/plus.svg" alt="" />
               </button>
             </div>
           </div>
 
           <div className="card">
+            <img className="favorite" src="./img/unliked.svg" alt="Unlikded" />
             <img width={133} height={122} src="./img/sneakers/4.jpg" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="d-flex justify-between">
@@ -82,7 +114,7 @@ function App() {
               </div>
               
               <button>
-                <img src="./img/plus.svg" alt="" />
+                <img className="plus" src="./img/plus.svg" alt="" />
               </button>
             </div>
           </div>
