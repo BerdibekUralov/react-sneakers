1345de6f1293af91b8700220bf045515dba57303
BerdibekUralov <uralov0212@gmail.com>
2022-11-09 08:58:29 +0300

    props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,29 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const arr = [
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede', 
+    price: 12999, 
+    imgUrl: './img/sneakers/1.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15600, 
+    imgUrl: './img/sneakers/2.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede', 
+    price: 8499, 
+    imgUrl: './img/sneakers/3.jpg'
+  },
+  {
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999, 
+    imgUrl: './img/sneakers/4.jpg'
+  },
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -21,11 +44,16 @@ function App() {
           </div>
         </div>
         
-        <div className="d-flex justify-between">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <div className="content-cards d-flex justify-between">
+          {
+            arr.map((obj) => (
+              <Card 
+                title={obj.title} 
+                price={obj.price} 
+                image={obj.imgUrl}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-const Card = () => {
+const Card = ({title, price, image}) => {
   return (
     <div className={styles.card}>
         <img className={styles.favorite} src="./img/unliked.svg" alt="Unlikded" />
-        <img width={133} height={122} src="./img/sneakers/1.jpg" alt="" />
-        <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+        <img width={133} height={122} src={image} alt="" />
+        <h5>{title}</h5>
         <div className="d-flex justify-between align-center">
             <div>
                 <span>Цена:</span><br/>
-                <b>12 999 руб.</b>
+                <b>{price} руб.</b>
             </div>
               
-            <button>
+            <button className='button'>
                 <img className={styles.plus} src="./img/plus.svg" alt="" />
             </button>
         </div>
